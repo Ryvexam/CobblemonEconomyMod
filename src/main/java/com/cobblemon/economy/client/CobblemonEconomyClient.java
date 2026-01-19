6478f844110b04a74bb025e23a1758408f4a24cf
Maxime VÉRY <maximevery@ryvexam.fr>
2026-01-19 18:18:50 +0100

    fix: use dedicated client logger to prevent classloading deadlock

--- src/main/java/com/cobblemon/economy/client/CobblemonEconomyClient.java
+++ src/main/java/com/cobblemon/economy/client/CobblemonEconomyClient.java
@@ -3,12 +3,16 @@ package com.cobblemon.economy.client;
 import com.cobblemon.economy.fabric.CobblemonEconomy;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class CobblemonEconomyClient implements ClientModInitializer {
+    public static final Logger CLIENT_LOGGER = LoggerFactory.getLogger("cobblemon-economy-client");
+
     @Override
     public void onInitializeClient() {
-        CobblemonEconomy.LOGGER.info("Starting Cobblemon Economy (Client Init)...");
+        CLIENT_LOGGER.info("Starting Cobblemon Economy (Client Init)...");
         EntityRendererRegistry.register(CobblemonEconomy.SHOPKEEPER, ShopkeeperRenderer::new);
-        CobblemonEconomy.LOGGER.info("Cobblemon Economy (Client Init) - DONE");
+        CLIENT_LOGGER.info("Cobblemon Economy (Client Init) - DONE");
     }
 }
