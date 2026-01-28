25060498e8671b7603553d5323ec210432ff8439
Guillaume Michel <GuillaumeMichel02@users.noreply.github.com>
2026-01-28 16:55:15 +0100

    Remove version handshake (handled by minecraft if needed)

--- src/main/java/com/cobblemon/economy/client/ClientVersionHandshake.java
+++ src/main/java/com/cobblemon/economy/client/ClientVersionHandshake.java
@@ -1,14 +1,19 @@
 package com.cobblemon.economy.client;
 
 import com.cobblemon.economy.networking.VersionCheckPayload;
-import com.cobblemon.economy.networking.VersionHandshake;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.fabricmc.loader.api.FabricLoader;
 
 public class ClientVersionHandshake {
     public static void registerClient() {
         ClientPlayConnectionEvents.JOIN.register((handler, sender, client) -> {
-            ClientPlayNetworking.send(new VersionCheckPayload(VersionHandshake.getModVersion()));
+            String version = FabricLoader.getInstance()
+                    .getModContainer("cobblemon-economy")
+                    .map(container -> container.getMetadata().getVersion().getFriendlyString())
+                    .orElse("unknown");
+            CobblemonEconomyClient.CLIENT_LOGGER.info("Sending version check to server: '{}'", version);
+            ClientPlayNetworking.send(new VersionCheckPayload(version));
         });
     }
 }
